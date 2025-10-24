6cc43656a6b705aaba6b64c2fef124291d2515f0
ssloke420 <131798522+ssloke420@users.noreply.github.com>
2025-10-23 20:38:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Admin Login Code
+let isAdmin = false;
+
+let submit = function() {
+    let entered = document.getElementsByName("psw")[0].value;
+    if (entered === "rhhs@2025") {
+        sessionStorage.setItem("isAdmin", "true");
+        document.getElementById("verify").innerHTML = "Login successful!";
+    } else {
+        document.getElementById("verify").innerHTML = "Incorrect Password";
+    }
+};
+
+test = function () {
+    
+         if (sessionStorage.getItem("isAdmin", "true")) {
+            document.getElementById("test").innerHTML = "is admin"
+        };
+};
+
