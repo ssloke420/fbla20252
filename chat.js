9c13fe0f389ae4fb257a4c5dcb7af46ae94bb690
ssloke420 <131798522+ssloke420@users.noreply.github.com>
2026-02-17 12:11:11 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -61,7 +61,7 @@ function isImageURL(url) {
 // Add message to chat
 function addMessage(message, username) {
     const chatMessages = document.getElementById('messages');
-    const timestamp = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    const timestamp = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: undefined });
     const messageContainer = document.createElement('div');
     messageContainer.classList.add('message-container');
 
